8a644769dcfe254f91108264a7c4ab9c32ae8b8a
wangjinbiao1985 <42571215+wangjinbiao1985@users.noreply.github.com>
2020-10-11 23:58:45 +0800

    修改
    修改

--- lib/flutter_cupertino_settings.dart
+++ lib/flutter_cupertino_settings.dart
@@ -36,9 +36,10 @@ const double CS_CHECK_SIZE = 28.0;
 /// Event for [CSSelection]
 typedef void SelectionCallback(int selected);
 
-TextStyle basicTextStyle(BuildContext context) => Platform.isIOS
-    ? CupertinoTheme.of(context).textTheme.textStyle
-    : Theme.of(context).textTheme.subhead;
+TextStyle basicTextStyle(BuildContext context) => //Platform.isIOS
+    //? 
+    CupertinoTheme.of(context).textTheme.textStyle;
+    //: Theme.of(context).textTheme.subhead;
 
 class CupertinoSettings extends StatelessWidget {
   final List<Widget> items;
